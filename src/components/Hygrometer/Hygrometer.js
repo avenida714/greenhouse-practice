181306d9e611498fc7c023a5a145d0eb0e4a79aa
Attiya <adarensburg@live.com>
2022-06-16 15:38:31 -0500

    phase 4 complete

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,14 +1,18 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
+import {ClimateContext} from '../../context/ClimateContext';
+import {useContext} from 'react';
 
 function Hygrometer() {
+  const {humidity, setHumidity} = useContext(ClimateContext);
+
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {"y"}%</div>
+      <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
+        value={humidity}
+        onAfterChange={(val) => {setHumidity(val.target.value)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -6,8 +6,9 @@ export const ClimateContext = createContext();
 
 export const ClimateProvider = (props) => {
     const [temperature, setTemperature] = useState(50);
+    const [humidity, setHumidity] = useState(40);
     return(
-        <ClimateContext.Provider value={{temperature, setTemperature}}>
+        <ClimateContext.Provider value={{temperature, setTemperature, humidity,setHumidity}}>
             {props.children}
         </ClimateContext.Provider>
     )
